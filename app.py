d462e86dffdd1f0223e7303e8c52dfb330eb5596
bilingwu <sarawu45@gmail.com>
2015-10-10 19:27:11 -0400

    keeping stuff logged in?

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template,request
-from flask import redirect, url_for
+from flask import Flask, render_template,request, session,redirect, url_for
 import sqlite3
 import csv
 
 app= Flask(__name__)
+loggedin= False
 
 @app.route("/")
 def default():
@@ -20,14 +20,17 @@ def login():
     else:
         uname=request.form['username']
         pword = request.form['password']
-        #button= request.form['button']
         if uname=="this" and pword=="that":
-            return "<h1>yey</h1> <hr>" +uname +pword
+            loggedin= True
+            return redirect(url_for('profile'))
         else:
             return(render_template("login.html") + uname + "<br>" +pword)
-            #print (uname + password)"""
-    
+            #print (uname + password)
 
+@app.route("/profile")
+def profile():
+    return "shhs"
+    
 if __name__ == "__main__":
     app.debug=True
     app.run(host="0.0.0.0",port=8000)
