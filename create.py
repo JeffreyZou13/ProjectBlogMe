d47b2e38e22b930df927225ed85aec850b9def18
JeffreyZou13 <jeffreyzou13@gmail.com>
2015-10-12 18:17:09 -0400

    Added the posts and comments csv, and added their tables to create.py

--- create.py
+++ create.py
@@ -1,12 +1,16 @@
 import sqlite3 
 import csv
 
-#Need to make the database name, fill it in for the empty strings
 conn = sqlite3.connect("blog.db")
 c = conn.cursor()
 
 q = "CREATE TABLE users(username text, password text)"
 c.execute(q)
+
+q = "CREATE TABLE posts(id int, username text, title text, post text)"
+c.execute(q)
+
+q = "CREATE TABLE comments(id int, username text, comment text)"
 
 conn.commit()
 
