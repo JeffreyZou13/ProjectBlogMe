e6a6c2e49de6e656ee69e6344f083ab68f0ec5f2
JeffreyZou13 <jeffreyzou13@gmail.com>
2015-10-13 23:14:29 -0400

    not sure of the securities stuff, might work

--- populate.py
+++ populate.py
@@ -6,27 +6,27 @@ c = conn.cursor()
 
 q = "delete from users"
 c.execute(q)
-BASE="INSERT INTO users VALUES('%(username)s','%(password)s')"
+BASE="INSERT INTO users VALUES(%s,%s)"
 for item in csv.DictReader(open("users.csv")):
     q = BASE%item
     print q
-    c.execute(q)
+    c.execute(q, (username, password))
 
 q = "delete from posts"
 c.execute(q)
-BASE="INSERT INTO posts VALUES('%(id)s','%(user)s','%(title)s','%(post)s')"
+BASE="INSERT INTO posts VALUES(%s,%s,%s,%s)"
 for item in csv.DictReader(open("posts.csv")):
     q = BASE%item
     print q
-    c.execute(q)
+    c.execute(q, (id, user, title, post))
 
 q = "delete from comments"
 c.execute(q)
-BASE="INSERT INTO comments VALUES('%(id)s','%(user)s','%(comment)s')"
+BASE="INSERT INTO comments VALUES(%s,%s,%s)"
 for item in csv.DictReader(open("comments.csv")):
     q = BASE%item
     print q
-    c.execute(q)
+    c.execute(q, (id, user, comment))
 
 
 conn.commit()
