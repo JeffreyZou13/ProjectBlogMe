d762363e00fff1c9436e64d7f5e19a634805a7b6
Ariel Levy <ariel.s.levy1@gmail.com>
2015-10-15 21:02:52 -0400

    added functions back to app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import csv
 import utils
 
 app= Flask(__name__)
-#loggedin= False
 
 @app.route("/")
 def default():
@@ -15,21 +14,44 @@ def home():
     return(render_template("home.html"))
 
 @app.route("/login", methods=["GET","POST"])
-def login():
+@app.route("/login/<message>", methods=["GET","POST"])
+def login(message=''):
     if request.method== "GET":
-        return(render_template("login.html"))
+        return(render_template("login.html",message=message))
     else:
         uname=request.form['username']
         pword = request.form['password']
-        if utils.authenticate(uname,pword): # checks if pword matches uname in database
-            #loggedin= True
+	if not utils.user_exists(uname):
+		return(render_template("login.html",message="User does not exist."))
+        elif utils.authenticate(uname,pword): # checks if pword matches uname in database
+	    session['user'] = uname # set current user 
             return redirect(url_for('profile', username=uname))
         else:
-            return(render_template("login.html") +uname+ "<br>" +pword)
-    		# return login.html with error message
+            return(render_template("login.html",message="Incorrect username or password."))
+
+@app.route("/register", methods=["GET","POST"])
+@app.route("/register/<message>", methods=["GET","POST"])
+def register():
+	if request.method == "GET":
+		return(render_template("register.html"))
+	else:
+		uname = request.form['username']
+		pword = request.form['password']
+		conf = request.form['confirm']
+		if utils.user_exists(uname):
+			return(render_template("register.html",message="Username already exists."))
+		elif len(pword) < 6:
+			return(render_template("register.html",message="Password should be at least 6 characters."))
+		elif pword != conf:
+			return(render_template("register.html",message="Password doesn't match confirmation."))
+		else:
+			utils.add_user(uname,pword)
+			return redirect(url_for('login'))
+	#form input for post
 
 @app.route("/logout") # redirects to login page
 def logout():
+	session['user'] = ''
 	return redirect(url_for("login"))
 
 @app.route("/profile")
@@ -42,12 +64,29 @@ def profile(username="Default"):
 @app.route("/post/<post_id>", methods=["GET","POST"])
 def post(post_id="1"):
 	if request.method == "POST":
-		comment = request.form['comment']
-		print comment
-		# add comment to database
+		if 'user' in session and session['user'] != '':
+			comment = request.form['comment']
+			# add comment to database
+			utils.add_comment(post_id,session['user'],comment)
+		else:
+			return redirect(url_for("login",message="Login before you post."))
 	d = utils.post_info(post_id)
 	comment_list = utils.comments(post_id)
 	return(render_template("post.html",d=d,comment_list=comment_list))
+        
+@app.route("/newpost", methods=["GET","POST"])
+def newpost():
+	if request.method == "GET":
+		if 'user' in session and session['user'] != '':
+			return(render_template("newpost.html"))
+		else:
+			return redirect(url_for("login",message="Login before you post."))
+	else:
+		title = request.form['title']
+		post = request.form['post']
+		# add post to database
+		utils.add_post(session['user'],title,post)
+		return redirect(url_for("post",post_id=1)) # redirect to new post
 
 @app.route("/posts", methods=["GET","POST"])
 def delete(post_id="None"):
@@ -59,18 +98,7 @@ def delete(post_id="None"):
         utils.delete_post(post_id)
         return render_template("/profile/<username>")
 
-        
-
-@app.route("/newpost", methods=["GET","POST"])
-def newpost():
-	if request.method == "GET":
-		return(render_template("newpost.html"))
-	else:
-		title = request.form['title']
-		post = request.form['post']
-		# add post to database
-		return redirect(url_for("post",post_id=1)) # redirect to new post
-
 if __name__ == "__main__":
     app.debug=True
+    app.secret_key = "allha1lthemajest1cplatyp1"
     app.run(host="0.0.0.0",port=5000)
